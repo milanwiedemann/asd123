80939314006700b8aa0a488409a8cbb11e29ac0a
Milan Wiedemann <milan.wiedemann@gmail.com>
2020-04-18 16:37:58 +0100

    updates, cleaning code a bit and adding examples to readme

--- R/create-var-names.R
+++ R/create-var-names.R
@@ -1,95 +1,16 @@
-#' Title
+#' Create variable names
 #'
-#' @param var_name 
-#' @param str1 
-#' @param n1 
-#' @param str2 
-#' @param n2 
-#' @param sort 
-#' @param name_index 
-#' @param unlist 
+#' @param var_name String, specifying the variable name.
+#' @param str String, specifying the suffix to be added after the \code{var_name}.
+#' @param n Numeric, specifying the number of variables.
+#' @param sep String, specifying the seperatpr between \code{var_name} and \code{str}.
+#' @param unlist Logical, specifying whether a list or vactor should be returned.
 #'
 #' @return
 #' @export
 #'
 #' @examples
-create_var_names2 <- function(var_name, str1 = "s", n1, str2 = "i", n2, sort = c("str1", "str2"), name_index = FALSE, unlist = FALSE) {
-  
-  # sort = c("previous", "current"), sort = c("base", "addition")
-  sort <- base::match.arg(sort)
-  
-  # Create string with variable names inclusing session ---
-  str1_temp <- purrr::map2(.x = base::rep(base::paste(var_name, str1, sep = "_"), n1), .y = 1:n1, .f = base::paste0)
-  str1_temp <-  base::unlist(str1_temp, use.names = FALSE)
-  
-  # Add items ----
-  # This should be a separate function so it could be applied outside of this function to add items, or whatever to the end of the sting to a list
-  # something like add_str() or add_suffix() or add_specifier()
-  str2_temp <- purrr::map2(.x = base::rep(base::paste(str1_temp, str2, sep = "_"), n2), .y = rep(1:n2, each = n1), .f = base::paste0)
-  str2_temp <-  base::unlist(str2_temp, use.names = FALSE)
-  
-  # Create list 1 ----
-  # one element per list for n1
-  return_list_1 <- list()
-  
-  # Create list 1 ----
-  for (i in seq_along(1:n1)) {
-    
-    if (name_index == FALSE) {
-      return_list_1[[i]] <- str2_temp[seq(1 + (i - 1), length(str2_temp), by = n1)]
-    } else if (name_index == TRUE) {
-      return_list_1[[paste0(var_name, "_", str1, i)]] <- str2_temp[seq(1 + (i - 1), length(str2_temp), by = n1)]
-    }
-  }
-  
-  # Create list 2 ----
-  # one element per list for each n2
-  return_list_2 <- list()
-  
-  for (i in seq_along(1:n2)) {
-    if (name_index == FALSE) {
-      return_list_2[[i]] <- str2_temp[seq(1, length(str2_temp), by = n1)[i]:(seq(1, length(str2_temp), by = n1)[i] + n1 - 1)]
-    } else if (name_index == TRUE) {
-      return_list_2[[paste0(var_name, "_", str2, i)]] <- str2_temp[seq(1, length(str2_temp), by = n1)[i]:(seq(1, length(str2_temp), by = n1)[i] + n1 - 1)]
-    }
-  }
-  
-  # Sort argument ----
-  # Return list sorted by n1 or n2
-  # Depending on unlist argument each sort element will be a separate list
-  if (sort == "str1") {
-    return_list <- return_list_1
-  } else if (sort == "str2") {
-    return_list <- return_list_2
-  }
-  
-  # Unlist argument ----
-  # No functionality to use names
-  if (unlist == TRUE) {
-    unlist(return_list, use.names = FALSE)
-  } else if (unlist == FALSE) {
-    return_list
-  }
-}
-
-
-
-
-
-
-
-#' Title
-#'
-#' @param var_name 
-#' @param str 
-#' @param n 
-#'
-#' @return
-#' @export
-#'
-#' @examples
-create_var_names <- function(var_name, str = "s", n, unlist = FALSE) {
-  
+create_var_names <- function(var_name, str = "s", n, sep = "_", unlist = FALSE) {
   
   # Create string with variable names inclusing session ---
   return_list <- purrr::map2(.x = base::rep(base::paste(var_name, str, sep = "_"), n), .y = 1:n, .f = base::paste0)
@@ -103,13 +24,9 @@ create_var_names <- function(var_name, str = "s", n, unlist = FALSE) {
   }
 }
 
-
-
-
-
-#' Title
+#' Add further specifications to variable names
 #'
-#' @param varnames 
+#' @param var_names List or vector of variable names 
 #' @param str 
 #' @param n 
 #' @param sort 
@@ -122,22 +39,16 @@ create_var_names <- function(var_name, str = "s", n, unlist = FALSE) {
 #' @examples
 add_specifier <- function(var_names, str = "i", n, sort = c("previous", "current"), name_index = FALSE, unlist = FALSE) {
   
+  # Check input of sort arguemtn
+  sort <- base::match.arg(sort)
+  
+  # Count length of input
+  # I need to change this so it can also count vectors within lists
   n_varnames <- length(var_names)
-  # sort = c("previous", "current"), sort = c("base", "addition")
-  # sort <- base::match.arg(sort)
-  # 
-  # # Create string with variable names inclusing session ---
-  # str1_temp <- purrr::map2(.x = base::rep(base::paste(var_name, str1, sep = "_"), n1), .y = 1:n1, .f = base::paste0)
-  # str1_temp <-  base::unlist(str1_temp, use.names = FALSE)
   
-  # Add items ----
-  # This should be a separate function so it could be applied outside of this function to add items, or whatever to the end of the sting to a list
-  # something like add_str() or add_suffix() or add_specifier()
-  str2_temp <- purrr::map2(.x = base::rep(base::paste(var_names, str, sep = "_"), n), .y = rep(1:n, each = n_varnames), .f = base::paste0)
-  str2_temp <-  base::unlist(str2_temp, use.names = FALSE)
-  
-  
-  # str2_temp
+  # Add string ----
+  str_temp <- purrr::map2(.x = base::rep(base::paste(var_names, str, sep = "_"), n), .y = rep(1:n, each = n_varnames), .f = base::paste0)
+  str_temp <-  base::unlist(str_temp, use.names = FALSE)
   
   # Create list 1 ----
   # one element per list for n_varnames
@@ -147,9 +58,9 @@ add_specifier <- function(var_names, str = "i", n, sort = c("previous", "current
   for (i in seq_along(1:n_varnames)) {
 
     if (name_index == FALSE) {
-      return_list_1[[i]] <- str2_temp[seq(1 + (i - 1), length(str2_temp), by = n_varnames)]
+      return_list_1[[i]] <- str_temp[seq(1 + (i - 1), length(str_temp), by = n_varnames)]
     } else if (name_index == TRUE) {
-      return_list_1[[paste0(var_name, "_", str1, i)]] <- str2_temp[seq(1 + (i - 1), length(str2_temp), by = n_varnames)]
+      return_list_1[[paste0(var_name, "_", str1, i)]] <- str_temp[seq(1 + (i - 1), length(str_temp), by = n_varnames)]
     }
   }
 
@@ -159,9 +70,9 @@ add_specifier <- function(var_names, str = "i", n, sort = c("previous", "current
 
   for (i in seq_along(1:n)) {
     if (name_index == FALSE) {
-      return_list_2[[i]] <- str2_temp[seq(1, length(str2_temp), by = n_varnames)[i]:(seq(1, length(str2_temp), by = n_varnames)[i] + n_varnames - 1)]
+      return_list_2[[i]] <- str_temp[seq(1, length(str_temp), by = n_varnames)[i]:(seq(1, length(str_temp), by = n_varnames)[i] + n_varnames - 1)]
     } else if (name_index == TRUE) {
-      return_list_2[[paste0(var_name, "_", str2, i)]] <- str2_temp[seq(1, length(str2_temp), by = n_varnames)[i]:(seq(1, length(str2_temp), by = n_varnames)[i] + n_varnames - 1)]
+      return_list_2[[paste0(var_name, "_", str2, i)]] <- str_temp[seq(1, length(str_temp), by = n_varnames)[i]:(seq(1, length(str_temp), by = n_varnames)[i] + n_varnames - 1)]
     }
   }
 
--- README.md
+++ README.md
@@ -11,7 +11,14 @@ status](https://ci.appveyor.com/api/projects/status/github/milanwiedemann/varnam
 status](https://travis-ci.com/milanwiedemann/varnames.svg?branch=master)](https://travis-ci.com/milanwiedemann/varnames)
 <!-- badges: end -->
 
-The goal of varnames is to …
+The goal of `varnames` is to help with creating consistent variable
+names. This has many advantages, even if no further calculations of the
+data are needed.
+
+Consistent variable names are particularly helpful when managing larger
+datasets. Below I some examples how consistent variable names help when
+combined with other useful tools like [Regular
+Expressions](https://stringr.tidyverse.org/articles/regular-expressions.html).
 
 ## Installation
 
@@ -31,63 +38,108 @@ devtools::install_github("milanwiedemann/varnames")
 
 ## Example
 
-This is a basic example which shows you how to solve a common problem:
+For most cases there might be a very simple solution in creating
+consistent variable names using the `paste()` function.
+
+``` r
+# Create vector of a measure with 3 time points (t)
+paste("measure_", "t", 1:3, sep = "")
+#> [1] "measure_t1" "measure_t2" "measure_t3"
+```
+
+Sometimes more than one specification need to be added to a variable
+name. Say something about “wide” data. Allthough this is not the best
+way to store data
+\[@Wickham2013\](<https://www.jstatsoft.org/article/view/v059i10>), it
+is often how data looks like. In the examples below I’m showing how data
+stored in these variable names can be transformend into “long” data.
+
+Similar to the example above, the `create_var_names()` function creates
+a vector (or list) of consistent variable names.
 
 ``` r
 library(varnames)
 library(tidyverse)
-#> ── Attaching packages ───────────────────────────────────────────────────────────────────────────────────── tidyverse 1.3.0 ──
+#> ── Attaching packages ───────────────────────────────────────────────────────────────────────────────────────────── tidyverse 1.3.0 ──
 #> ✓ ggplot2 3.3.0     ✓ purrr   0.3.3
 #> ✓ tibble  3.0.0     ✓ dplyr   0.8.5
 #> ✓ tidyr   1.0.2     ✓ stringr 1.4.0
 #> ✓ readr   1.3.1     ✓ forcats 0.5.0
-#> ── Conflicts ──────────────────────────────────────────────────────────────────────────────────────── tidyverse_conflicts() ──
+#> ── Conflicts ──────────────────────────────────────────────────────────────────────────────────────────────── tidyverse_conflicts() ──
 #> x dplyr::filter() masks stats::filter()
 #> x dplyr::lag()    masks stats::lag()
-## basic example code
 
-create_var_names(var_name = "pds", str = "s", n = 2, unlist = T) %>% 
-  add_specifier(str = "i", n = 3, sort = "previous", unlist = F) 
+# Create vector of a measure with 3 time points (t)
+create_var_names(var_name = "measure", str = "t", n = 3)
 #> [[1]]
-#> [1] "pds_s1_i1" "pds_s1_i2" "pds_s1_i3"
+#> [1] "measure_t1"
 #> 
 #> [[2]]
-#> [1] "pds_s2_i1" "pds_s2_i2" "pds_s2_i3"
-
-create_var_names(var_name = "pds", str = "s", n = 2, unlist = T) %>% 
-  add_specifier(str = "i", n = 3, sort = "current", unlist = T) %>% 
-  add_specifier(str = "t", n = 4, sort = "current", unlist = F)
-#> [[1]]
-#> [1] "pds_s1_i1_t1" "pds_s2_i1_t1" "pds_s1_i2_t1" "pds_s2_i2_t1" "pds_s1_i3_t1"
-#> [6] "pds_s2_i3_t1"
-#> 
-#> [[2]]
-#> [1] "pds_s1_i1_t2" "pds_s2_i1_t2" "pds_s1_i2_t2" "pds_s2_i2_t2" "pds_s1_i3_t2"
-#> [6] "pds_s2_i3_t2"
+#> [1] "measure_t2"
 #> 
 #> [[3]]
-#> [1] "pds_s1_i1_t3" "pds_s2_i1_t3" "pds_s1_i2_t3" "pds_s2_i2_t3" "pds_s1_i3_t3"
-#> [6] "pds_s2_i3_t3"
-#> 
-#> [[4]]
-#> [1] "pds_s1_i1_t4" "pds_s2_i1_t4" "pds_s1_i2_t4" "pds_s2_i2_t4" "pds_s1_i3_t4"
-#> [6] "pds_s2_i3_t4"
+#> [1] "measure_t3"
 ```
 
-Just realised something like this may be way simpler
+The `add_specifier()` function adds a specifier, here “i” to the
+variables created in the step before. A feature of this function is that
+variable names can be returend in different ways, depending on what
+output format is most suitable for further use.
+
+The example below adds two indicators “i” for each time point “t” and
+returns a list that is sorted by the specifyer added created initially
+“t”.
 
 ``` r
-library(tidyverse)
-paste0("pds", "_", "s", 1:10) %>% 
-  paste0("_",  "i", 1:4)
-#>  [1] "pds_s1_i1"  "pds_s2_i2"  "pds_s3_i3"  "pds_s4_i4"  "pds_s5_i1" 
-#>  [6] "pds_s6_i2"  "pds_s7_i3"  "pds_s8_i4"  "pds_s9_i1"  "pds_s10_i2"
+create_var_names(var_name = "measure", str = "t", n = 3) %>% 
+  add_specifier(str = "i", n = 2, sort = "previous")
+#> [[1]]
+#> [1] "measure_t1_i1" "measure_t1_i2"
+#> 
+#> [[2]]
+#> [1] "measure_t2_i1" "measure_t2_i2"
+#> 
+#> [[3]]
+#> [1] "measure_t3_i1" "measure_t3_i2"
 ```
 
-### Create variable names
+The output list can also be sorted according to the second specifyer
+that is added by using the argument `sort = "current"`. Now a list with
+two elements gets returned, one for each indicator that was added.
 
-### Rename variables
+``` r
+create_var_names(var_name = "measure", str = "t", n = 3) %>% 
+  add_specifier(str = "i", n = 2, sort = "current")
+#> [[1]]
+#> [1] "measure_t1_i1" "measure_t2_i1" "measure_t3_i1"
+#> 
+#> [[2]]
+#> [1] "measure_t1_i2" "measure_t2_i2" "measure_t3_i2"
+```
 
-## Why is this so useful?
+If the desired output is a vactor of all variables the argument `unlist
+= TRUE` can be used.
 
-  - Show some examples, regular expressions automation etc
+``` r
+create_var_names(var_name = "measure", str = "t", n = 3) %>% 
+  add_specifier(str = "i", n = 2, sort = "current", unlist = TRUE)
+#> [1] "measure_t1_i1" "measure_t2_i1" "measure_t3_i1" "measure_t1_i2"
+#> [5] "measure_t2_i2" "measure_t3_i2"
+```
+
+The order of the vector can still be changed by the sort argument.
+
+``` r
+create_var_names(var_name = "measure", str = "t", n = 3) %>% 
+  add_specifier(str = "i", n = 2, sort = "previous", unlist = TRUE)
+#> [1] "measure_t1_i1" "measure_t1_i2" "measure_t2_i1" "measure_t2_i2"
+#> [5] "measure_t3_i1" "measure_t3_i2"
+```
+
+## TODOS
+
+  - Add Why is this so useful? Show some examples, regular expressions
+    automation etc
+  - Add rename functions
+  - Currently this only works when adding one specifyer, I think more
+    would be cool, I might need help with this
