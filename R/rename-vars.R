b17a0b214f4982a3ad14dd0145638699d8d9c81d
Milan Wiedemann <milan.wiedemann@gmail.com>
2020-05-04 21:47:36 +0100

    add rename function

---
+++ R/rename-vars.R
@@ -0,0 +1,65 @@
+#' Rename item variables
+#' @description Rename item variables consistently, this works only for variables named InMeasure1 which stands for Item 1 at Intake Assessment for Measure, or Session1Measure5 wich stands for Item 5 at Session 1 of Measure
+#' @param data Dataset
+#' @param id_str String, id variable
+#' @param session Numeric or String identifying session/timepoint
+#' @param data_str_measure Sting, unique string identifying measure
+#' @param prefix_data_str_session String pattern in data that identifies session/timepoint
+#' @param new_str_measure String, if unhappy with name of measure in data, you can change it here
+#' @param prefix_new_str_session String, idicator for session/timepoint, default "s"
+#' @param prefix_new_str_item String, idicator for item, default "i"
+#'
+#' @export
+#'
+rename_item_vars <- function(data, id_str, data_session, new_session = data_session, data_str_measure, prefix_data_str_session, new_str_measure = data_str_measure, prefix_new_str_session = "s", prefix_new_str_item = "i"){
+  
+  # This part is for all numeric sessions
+  if (base::is.numeric(data_session) == TRUE) {
+    
+    string_search <- base::paste(prefix_data_str_session, data_session, data_str_measure, "\\d", sep = "")
+    
+    data_select <- dplyr::select(data, id_str, matches(string_search))
+    
+    original_varibale_names <- base::names(dplyr::select(data, matches(string_search)))
+    
+    string_item_number_search <- base::paste("(?<=", data_str_measure, ")\\d+", sep = "")
+    
+    item_number <- stringr::str_extract(original_varibale_names, string_item_number_search)
+    
+    session_number <- stringr::str_extract(original_varibale_names, paste("(?<=", prefix_data_str_session, ")\\d+", sep = ""))
+    
+    new_variables_names <- paste(stringr::str_to_lower(new_str_measure), "_", prefix_new_str_session, session_number, "_", prefix_new_str_item, item_number, sep = "")
+    
+    id_new_variables_names <- c(id_str, new_variables_names)
+    
+    names(data_select) <- id_new_variables_names
+    
+    return(data_select)
+    
+    # This part is for all numeric sessions
+  } else if (base::is.character(data_session) == TRUE) {
+    
+    string_search <- paste(data_session, data_str_measure, "\\d", sep = "")
+    
+    data_select <- dplyr::select(data, id_str, matches(string_search))
+    
+    original_varibale_names <- names(dplyr::select(data, matches(string_search)))
+    
+    string_item_number_search <- paste("(?<=", data_str_measure, ")\\d+", sep = "")
+    
+    item_number <- stringr::str_extract(original_varibale_names, string_item_number_search)
+    
+    session_number <- stringr::str_to_lower(new_session)
+    
+    new_variables_names <- paste(stringr::str_to_lower(new_str_measure), "_", session_number, "_" , prefix_new_str_item, item_number, sep = "")
+    
+    id_new_variables_names <- c(id_str, new_variables_names)
+    
+    names(data_select) <- id_new_variables_names
+    
+    return(data_select)
+    
+  } else {
+    stop("BOOOOM ERROR \n something wrong with data_session input.")
+  }
+}
--- README.md
+++ README.md
@@ -60,12 +60,12 @@ the arguments `str = "t"` and `n = 3`.
 ``` r
 library(varnames)
 library(tidyverse)
-#> ── Attaching packages ───────────────────────────────────────────────────────────────────────────────────────────── tidyverse 1.3.0 ──
-#> ✓ ggplot2 3.3.0     ✓ purrr   0.3.3
-#> ✓ tibble  3.0.0     ✓ dplyr   0.8.5
+#> ── Attaching packages ────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────── tidyverse 1.3.0 ──
+#> ✓ ggplot2 3.3.0     ✓ purrr   0.3.4
+#> ✓ tibble  3.0.1     ✓ dplyr   0.8.5
 #> ✓ tidyr   1.0.2     ✓ stringr 1.4.0
 #> ✓ readr   1.3.1     ✓ forcats 0.5.0
-#> ── Conflicts ──────────────────────────────────────────────────────────────────────────────────────────────── tidyverse_conflicts() ──
+#> ── Conflicts ───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────── tidyverse_conflicts() ──
 #> x dplyr::filter() masks stats::filter()
 #> x dplyr::lag()    masks stats::lag()
 
@@ -125,15 +125,23 @@ by changing the sort argument.
 
 ``` r
 create_var_names(var_name = "measure", str = "t", n = 3) %>% 
-  add_specifier(str = "i", n = 2, sort = "previous", unlist = TRUE)
-#> [1] "measure_t1_i1" "measure_t1_i2" "measure_t2_i1" "measure_t2_i2"
-#> [5] "measure_t3_i1" "measure_t3_i2"
+  add_specifier(str = "i", n = 2, sort = "previous", unlist = F)
+#> [[1]]
+#> [1] "measure_t1_i1" "measure_t1_i2"
+#> 
+#> [[2]]
+#> [1] "measure_t2_i1" "measure_t2_i2"
+#> 
+#> [[3]]
+#> [1] "measure_t3_i1" "measure_t3_i2"
 ```
 
 ## TODOs
 
   - Say something about “tidy” data
-    \[@Wickham2013\](<https://www.jstatsoft.org/article/view/v059i10>)
+    [Wickham2013](https://www.jstatsoft.org/article/view/v059i10)
+  - Take a closer look at the bad data guide
+    <https://github.com/Quartz/bad-data-guide>
   - In the examples below I’m showing how data stored in these variable
     names can be transformend into “long” data.
   - Add Why is this so useful? Show some examples, regular expressions
